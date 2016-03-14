0374ca01c7ccdef3fc3d399377402b62c8af7a12
Sean Toner <stoner@localhost.localdomain>
2016-03-14 07:24:54 -0400

    Can just use a jar file now rather than embedded in a project

--- src/main/java/com/redhat/qe/rhsm/JarHelper.java
+++ src/main/java/com/redhat/qe/rhsm/JarHelper.java
@@ -1,14 +1,15 @@
 package com.redhat.qe.rhsm;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
+
 
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
@@ -19,10 +20,15 @@ import joptsimple.OptionSet;
  * Takes a jar file from the classpath,
  */
 public class JarHelper {
-    ArrayList<URL> urls;
+    List<URL> jarPaths;
+    String paths;
 
-    public JarHelper() {
-        this.urls = new ArrayList<>();
+    public JarHelper(String paths) {
+        this.jarPaths = JarHelper.convertToUrl(paths);
+        this.paths = this.jarPaths.stream()
+                .map( u -> u.getFile() )
+                .reduce("", (i, c) -> c + "," + i);
+        System.out.println("In constructor: " + this.paths);
     }
 
 
@@ -45,10 +51,50 @@ public class JarHelper {
     }
 
 
-    public void loader (String className) {
-        
+    public URLClassLoader makeLoader () {
+        //List<String> jars = new ArrayList<>(Arrays.asList(jarNames));
+        List<URL> urls = this.jarPaths;
+        return new URLClassLoader(urls.toArray(new URL[urls.size()]));
     }
 
+
+    public Reflector loadClasses(List<String> classes) {
+        URLClassLoader ucl = this.makeLoader();
+        Reflector refl = new Reflector();
+        for(String s: classes) {
+            try {
+                Class<?> cls = ucl.loadClass(s);
+                refl.getAnnotations(cls);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return refl;
+    }
+
+
+    /**
+     * Takes a possibly comma separated string of paths and converts it to a List of URLs
+     *
+     * @param paths
+     * @return
+     */
+    public static List<URL> convertToUrl(String paths) {
+        ArrayList<String> jars = new ArrayList<>(Arrays.asList(paths.split(",")));
+        List<URL> jarUrls = jars.stream()
+                .map(j -> {
+                    URL url = null;
+                    System.out.println(j);
+                    try {
+                        url = new URL(j);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    return url;
+                })
+                .collect(Collectors.toList());
+        return jarUrls;
+    }
 
     /**
      * Takes the jars (which must also be on the classpath) and the names of packages
@@ -61,12 +107,17 @@ public class JarHelper {
         parser.accepts("packages").withRequiredArg();
 
         OptionSet opts = parser.parse(args);
-        String jarName = (String) opts.valueOf("jars");
+        String jarPathsOpt = (String) opts.valueOf("jars");
         String packName = (String) opts.valueOf("packages");
 
+        JarHelper jh = new JarHelper(jarPathsOpt);
         try {
-            List<String> classes = JarHelper.loadJar(jarName, packName);
-            classes.forEach(System.out::println);
+            for(String s: jh.paths.split(",")) {
+                List<String> classes = JarHelper.loadJar(s, packName);
+                classes.forEach(System.out::println);
+                Reflector refl = jh.loadClasses(classes);
+                refl.showMap();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/redhat/qe/rhsm/Reflector.java
+++ src/main/java/com/redhat/qe/rhsm/Reflector.java
@@ -16,9 +16,11 @@ import org.testng.annotations.Test;
 public class Reflector {
 
     public HashMap<String, ArrayList<String>> testsToClasses;
+    private Set<String> testTypes;
 
     public Reflector(){
         testsToClasses = new HashMap<>();
+        testTypes = new HashSet<>(Arrays.asList("AcceptanceTests", "Tier1Tests", "Tier2Tests", "Tier3Tests"));
     }
 
     public interface GetGroups<T> {
@@ -30,8 +32,12 @@ public class Reflector {
     }
 
 
-    public static void main(String[] args) {
-        Reflector reflector = new Reflector();
+    /**
+     *
+     * @param args
+     */
+    public void reflect(String[] args) {
+        //Reflector reflector = new Reflector();
 
         /**
          * We have 4 categories of tests:
@@ -54,35 +60,42 @@ public class Reflector {
 
             Set<Class<?>> classes = refl.getTypesAnnotatedWith(Test.class);
             for (Class<?> c : classes) {
-                Method[] methods = c.getMethods();
-                // Get the groups from the Test annotation, store it in a set
-                Annotation ann = c.getAnnotation(Test.class);
-                String[] groups = ((Test) ann).groups();
-                Set<String> groupSet = new TreeSet<>(Arrays.asList(groups));
-
-                // Get only the groups from testTypes
-                groupSet = groupSet.stream()
-                        .filter(testTypes::contains)
-                        .collect(Collectors.toSet());
-
-                // TODO: Figure out how to turn this into a stream using a reduce or collect to
-                // store this to a Map
-                // For each item in groupSet, add the group to the groupsToClass map as the key, and as the value
-                // add an entry to the ArrayList
-                for (String g : groupSet) {
-                    if (!reflector.testsToClasses.containsKey(g)) {
-                        ArrayList<String> classNames = new ArrayList<>();
-                        classNames.add(c.getName());
-                        reflector.testsToClasses.put(g, classNames);
-                    } else {
-                        reflector.testsToClasses.get(g).add(c.getName());
-                    }
-                }
+                this.getAnnotations(c);
             }
         }
+        this.showMap();
+    }
 
-        reflector.testsToClasses.entrySet()
+    public void showMap() {
+        this.testsToClasses.entrySet()
                 .stream()
                 .forEach((es) -> System.out.println(es.getKey() + "=" + es.getValue()));
+    }
+
+    public <T> void getAnnotations(Class<T> c) {
+        Method[] methods = c.getMethods();
+        // Get the groups from the Test annotation, store it in a set
+        Annotation ann = c.getAnnotation(Test.class);
+        String[] groups = ((Test) ann).groups();
+        Set<String> groupSet = new TreeSet<>(Arrays.asList(groups));
+
+        // Get only the groups from testTypes
+        groupSet = groupSet.stream()
+                .filter(testTypes::contains)
+                .collect(Collectors.toSet());
+
+        // TODO: Figure out how to turn this into a stream using a reduce or collect to
+        // store this to a Map
+        // For each item in groupSet, add the group to the groupsToClass map as the key, and as the value
+        // add an entry to the ArrayList
+        for (String g : groupSet) {
+            if (!this.testsToClasses.containsKey(g)) {
+                ArrayList<String> classNames = new ArrayList<>();
+                classNames.add(c.getName());
+                this.testsToClasses.put(g, classNames);
+            } else {
+                this.testsToClasses.get(g).add(c.getName());
+            }
+        }
     }
 }
