b6f9de3e7dd7fd6f7d4ba601514ff38ccc232afd
Sean Toner <stoner@localhost.localdomain>
2016-06-22 16:39:58 -0400

    created a stub class with @Polarion annotation

---
+++ src/main/java/com/redhat/qe/rhsm/TestAnnotation.java
@@ -0,0 +1,15 @@
+package com.redhat.qe.rhsm;
+
+
+import com.redhat.qe.rhsm.metadata.Polarion;
+import com.redhat.qe.rhsm.metadata.Requirement;
+
+/**
+ * Created by stoner on 6/22/16.
+ */
+@Polarion(projectID = "Sean Toner",
+          requirement = @Requirement(polarionId = "",
+                                     config = "/path/to/xmlConfig.xml",
+                                     feature = ""))
+public class TestAnnotation {
+}
