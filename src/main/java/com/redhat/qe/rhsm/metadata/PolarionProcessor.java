c6e14e2c8015c3a21001ec14adbc0dacd012969a
Sean Toner <stoner@localhost.localdomain>
2016-06-24 15:56:40 -0400

    Adding functionality to PolarionProcessor

--- src/main/java/com/redhat/qe/rhsm/metadata/PolarionProcessor.java
+++ src/main/java/com/redhat/qe/rhsm/metadata/PolarionProcessor.java
@@ -1,31 +1,73 @@
 package com.redhat.qe.rhsm.metadata;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
  * Created by stoner on 5/16/16.
  */
 public class PolarionProcessor extends AbstractProcessor {
+    private Types types;
+    private Elements elements;
+    private Messager msgr;
+    private Filer filer;
+
+    /**
+     * The PolarionProcessor actually needs to look for three annotation types:
+     * - @Polarion: to get TestCase WorkItem information
+     * - @Requirement: to get Requirement WorkItem information
+     * - @Test: to get the existing description
+     *
+     * @param set
+     * @param roundEnvironment
+     * @return
+     */
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        String iface = Requirement.class.getSimpleName();
+        roundEnvironment.getElementsAnnotatedWith(Requirement.class).stream()
+                .map(ae -> {
+                    if (ae.getKind() != ElementKind.METHOD ||
+                        ae.getKind() != ElementKind.CLASS) {
+                        this.errorMsg(ae, "Can only annotate classes or methods with @%s", iface);
+                    }
+                    return ae;
+                 })
+                .filter(ae -> ae.getKind() == ElementKind.METHOD);
         return false;
     }
 
     @Override
-    public synchronized void init(ProcessingEnvironment env) {}
+    public synchronized void init(ProcessingEnvironment env) {
+        super.init(env);
+        this.types = env.getTypeUtils();
+        this.elements = env.getElementUtils();
+        this.filer = env.getFiler();
+        this.msgr = env.getMessager();
+    }
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        return null;
+        Set<String> anns = new LinkedHashSet<>();
+        anns.add(Polarion.class.getCanonicalName());
+        anns.add(Requirement.class.getCanonicalName());
+        return anns;
     }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
-        return null;
+        return SourceVersion.latestSupported();
+    }
+
+    private void errorMsg(Element elem, String msg, Object... args) {
+        this.msgr.printMessage(Diagnostic.Kind.ERROR, String.format(msg, args), elem);
     }
 }
