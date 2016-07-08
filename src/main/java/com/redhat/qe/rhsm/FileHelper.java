e05b6f201689210c561e64d27f293f0ee451a6a2
Sean Toner <stoner@redhat.com>
2016-07-08 15:03:14 -0400

    More functions for FileHelper

--- src/main/java/com/redhat/qe/rhsm/FileHelper.java
+++ src/main/java/com/redhat/qe/rhsm/FileHelper.java
@@ -1,12 +1,15 @@
 package com.redhat.qe.rhsm;
 
+import java.lang.annotation.Annotation;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.File;
-import java.util.List;
 import java.util.Optional;
+import com.redhat.qe.rhsm.metadata.Meta;
+import com.redhat.qe.rhsm.metadata.Polarion;
+import com.redhat.qe.rhsm.metadata.Requirement;
 
 /**
  * Created by stoner on 7/7/16.
@@ -20,5 +23,43 @@ public class FileHelper {
 
         }
         return Optional.of(p);
+    }
+
+    public static boolean pathExists(Path p) {
+        return p.toFile().exists();
+    }
+
+    /**
+     * Creates a Path to look up or create an xml description
+     *
+     * It uses this pattern:
+     *
+     * base/[project]/requirements|testcases/[class]/[methodName].xml as the path to the File
+     *
+     * @param base
+     * @param meta
+     * @return
+     */
+    public static <T> Path makeXmlPath(String base, Meta<T> meta) throws InvalidArgumentType {
+        String proj;
+        String type;
+        if (meta.annotation instanceof Polarion) {
+            Polarion p = (Polarion) meta.annotation;
+            proj = p.projectID();
+            type = "testcases";
+        }
+        else if (meta.annotation instanceof Requirement){
+            Requirement r = (Requirement) meta.annotation;
+            proj = r.project();
+            type = "requirements";
+        }
+        else
+            throw new InvalidArgumentType();
+
+        Path basePath = Paths.get(base, proj, type, meta.className);
+        String fullPath = basePath.toString() + meta.methName + ".xml";
+        Path path = Paths.get(fullPath);
+
+        return path;
     }
 }
