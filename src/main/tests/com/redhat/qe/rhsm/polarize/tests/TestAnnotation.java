9e4c86b37a603e3f0adb1bb72c8abd14fde08414
Sean Toner <stoner@localhost.localdomain>
2016-09-16 16:28:56 -0400

    Start writing some unit tests

--- src/main/tests/com/redhat/qe/rhsm/polarize/tests/TestAnnotation.java
+++ src/main/tests/com/redhat/qe/rhsm/polarize/tests/TestAnnotation.java
@@ -4,4 +4,5 @@ package com.redhat.qe.rhsm.polarize.tests;
  * Created by stoner on 9/15/16.
  */
 public class TestAnnotation {
+
 }
--- src/main/tests/com/redhat/qe/rhsm/polarize/tests/TestRequirements.java
+++ src/main/tests/com/redhat/qe/rhsm/polarize/tests/TestRequirements.java
@@ -1,12 +1,8 @@
 package com.redhat.qe.rhsm.polarize.tests;
 
-import cucumber.api.java8.En;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
 
 /**
- * Created by stoner on 7/7/16.
+ *
  */
 public class TestRequirements {
 
