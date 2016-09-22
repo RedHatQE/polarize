4dea9b8ea57bc7c2a4d8f187495ea33838c8df9a
Sean Toner <stoner@localhost.localdomain>
2016-09-22 10:41:55 -0400

    Starting to write cucumber style tests

--- src/main/test/java/com/redhat/qe/rhsm/polarize/tests/TestImporter.java
+++ src/main/test/java/com/redhat/qe/rhsm/polarize/tests/TestImporter.java
@@ -1,10 +1,12 @@
 package com.redhat.qe.rhsm.polarize.tests;
 
-import 
+import cucumber.api.java8.En;
 
 /**
  * Created by stoner on 9/15/16.
  */
-public class TestAnnotation {
-
+public class TestImporter implements En {
+    public TestImporter() {
+        Given("")
+    }
 }
