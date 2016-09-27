7bfe27d8fe1c74c5627ac72744d3641e2f0cc18e
Sean Toner <stoner@localhost.localdomain>
2016-09-27 16:08:53 -0400

    Removing all the Requirements related code

---
+++ src/main/java/com/github/redhatqe/polarize/Req.java
@@ -0,0 +1,7 @@
+package com.github.redhatqe.polarize;
+
+/**
+ * Created by stoner on 9/27/16.
+ */
+public class Requirements {
+}
---
+++ src/main/java/com/github/redhatqe/polarize/utils/Consumer2.java
@@ -0,0 +1,7 @@
+package com.github.redhatqe.polarize.utils;
+
+/**
+ * Created by stoner on 9/23/16.
+ */
+public interface Consumer2 {
+}
