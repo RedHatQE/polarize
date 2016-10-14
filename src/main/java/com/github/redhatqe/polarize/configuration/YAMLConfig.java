4969b7ef072c66822039b236f349501efd77f99a
Sean Toner <stoner@localhost.localdomain>
2016-10-14 11:53:55 -0400

    Publishing for build.gradle working

---
+++ src/main/java/com/github/redhatqe/polarize/configuration/YAMLConfig.java
@@ -0,0 +1,7 @@
+package com.github.redhatqe.polarize.configuration;
+
+/**
+ * Created by stoner on 10/13/16.
+ */
+public class YAMLConfig {
+}
---
+++ src/main/java/com/github/redhatqe/polarize/messagebus/CIBusPublisher.java
@@ -0,0 +1,7 @@
+package com.github.redhatqe.polarize.messagebus;
+
+/**
+ * Created by stoner on 10/12/16.
+ */
+public class CIBusPublisher {
+}
