a47bf5203373ea4953da04c4ebfc486f4207610b
Sean Toner <stoner@localhost.localdomain>
2016-10-17 23:16:52 -0400

    Adding XMLConfig to do configuration via XML

---
+++ src/main/java/com/github/redhatqe/polarize/configuration/XMLConfig.java
@@ -0,0 +1,130 @@
+package com.github.redhatqe.polarize.configuration;
+
+import com.github.redhatqe.polarize.IJAXBHelper;
+import com.github.redhatqe.polarize.JAXBHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Created by stoner on 10/17/16.
+ */
+public class XMLConfig {
+    public ConfigType config;
+    public Map<String, ImporterType> importers = new HashMap<>();
+    public Map<String, String> customFields = new HashMap<>();
+    public Map<String, String> testRunProps = new HashMap<>();
+    public ServerType polarion;
+    public ServerType kerb;
+    public ServerType ossrh;
+    public ImporterType xunit;
+    public ImporterType testcase;
+    public Logger logger = LoggerFactory.getLogger(XMLConfig.class);
+
+    public XMLConfig(File path) {
+        if (path == null) {
+            String homeDir = System.getProperty("user.home");
+            path = FileSystems.getDefault().getPath(homeDir + "/.polarize/yaml-config.yml").toFile();
+        }
+        JAXBHelper jaxb = new JAXBHelper();
+        Optional<ConfigType> maybeCfg;
+        maybeCfg = IJAXBHelper.unmarshaller(ConfigType.class, path, jaxb.getXSDFromResource(ConfigType.class));
+        if (!maybeCfg.isPresent()) {
+            throw new Error("Could not load configuration file");
+        }
+        this.config = maybeCfg.get();
+        this.initImporters();
+        xunit = this.importers.get("xunit");
+        testcase = this.importers.get("testcase");
+
+        this.initCustomFields();
+        this.initTestSuite();
+        this.initServers();
+    }
+
+    private void initServers() {
+        List<ServerType> servers = this.config.getServers().getServer();
+        for(ServerType st: servers) {
+            switch(st.getName()) {
+                case "polarion":
+                    this.polarion = st;
+                    break;
+                case "kerberos":
+                    this.kerb = st;
+                    break;
+                case "ossrh":
+                    this.ossrh = st;
+                    break;
+                default:
+                    this.logger.error("Unknown server type: %s", st.getName());
+            }
+        }
+    }
+
+    private void initImporters() {
+        List<ImporterType> importers = this.config.importers.getImporter();
+        importers.forEach(i -> this.importers.put(i.type, i));
+    }
+
+    public List<PropertyType> getCustomFields() {
+        CustomFieldsType fields = xunit.getCustomFields();
+        return fields.getProperty();
+    }
+
+    /**
+     * Given the customFields map, put this into the CustomFieldsType
+     */
+    public void setCustomFields() {
+        CustomFieldsType cft = this.xunit.getCustomFields();
+        List<PropertyType> props = cft.getProperty();
+        props.clear();
+        props.addAll(this.customFields.entrySet().stream()
+                .map(es -> {
+                    PropertyType pt = new PropertyType();
+                    pt.setName(es.getKey());
+                    pt.setVal(es.getValue());
+                    return pt;
+                })
+                .collect(Collectors.toList()));
+    }
+
+    public void initCustomFields() {
+        List<PropertyType> props = this.getCustomFields();
+        props.forEach(p -> this.customFields.put(p.getName(), p.getVal()));
+    }
+
+    public List<PropertyType> getTestSuite() {
+        TestSuiteType tProps = xunit.getTestSuite();
+        return tProps.getProperty();
+    }
+
+    public void setTestSuite() {
+        TestSuiteType tst = this.xunit.getTestSuite();
+        List<PropertyType> props = tst.getProperty();
+        props.clear();
+        props.addAll(this.testRunProps.entrySet().stream()
+                .map(es -> {
+                    PropertyType pt = new PropertyType();
+                    pt.setName(es.getKey());
+                    pt.setVal(es.getValue());
+                    return pt;
+                })
+                .collect(Collectors.toList()));
+    }
+
+    public void initTestSuite() {
+        List<PropertyType> props = this.getTestSuite();
+        props.forEach(p -> this.testRunProps.put(p.getName(), p.getVal()));
+    }
+
+    public static void main(String[] args) {
+        XMLConfig xml = new XMLConfig(new File("/home/stoner/.polarize/xml-config.xml"));
+    }
+}
--- src/main/java/com/github/redhatqe/polarize/configuration/YAMLConfig.java
+++ src/main/java/com/github/redhatqe/polarize/configuration/YAMLConfig.java
@@ -1,77 +1,230 @@
 package com.github.redhatqe.polarize.configuration;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.github.redhatqe.polarize.junitreporter.ReporterConfig;
 import com.github.redhatqe.polarize.metadata.DefTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by stoner on 10/13/16.
  */
 public class YAMLConfig {
+    @JsonProperty(value="polarion")
     public Server polarionServer;
+    @JsonProperty(value="kerberos")
     public Server kerbServer;
+    @JsonProperty(value="ossrh")
     public Server ossrhServer;
+    @JsonProperty(value="testcases-xml")
     public String testcasesXMLPath;
+    @JsonProperty(value="requirements-xml")
     public String requirementsXMLPath;
+    @JsonProperty(value="mapping")
     public String mappingPath;
+    @JsonProperty(value="testcase")
     public TestCaseImporter tcImporter;
+    @JsonProperty(value="xunit")
     public XUnitImporter xunitImporter;
+
     public String author;
     public String project;
+    @JsonIgnore
     public Logger logger;
 
-    public Boolean setDryRun;
 
     public YAMLConfig() {
-        this.tcImporter = new TestCaseImporter();
-        this.xunitImporter = new XUnitImporter();
         this.logger = LoggerFactory.getLogger(YAMLConfig.class);
     }
 
-    class Server {
+    public class Server {
         public String url = "";
         public String user;
         public String password;
     }
 
-    abstract class Importer {
+    public class CustomProps {
+        public Boolean dryRun;
+        public Boolean setTestRunFinished;
+        public Boolean includeSkipped;
+    }
+
+    public class ImportType {
+        @JsonProperty(value="testcase")
+        public TestCaseImporter testcase;
+        @JsonProperty(value="xunit")
+        public XUnitImporter xunit;
+
+        public ImportType() {
+            YAMLConfig.this.logger.info("In ImportType constructor");
+        }
+    }
+
+    @JsonProperty(value="importer")
+    public void setImportType(ImportType it) {
+        this.tcImporter = it.testcase;
+        this.xunitImporter = it.xunit;
+    }
+
+    public class Endpoint {
+        public String endpoint;
+
+        public Endpoint(String ep) {
+            endpoint = ep;
+        }
+
+        @JsonProperty(value="endpoint")
+        public void setEndpoint(Endpoint ep) {
+            endpoint = ep.endpoint;
+        }
+    }
+
+    public class FileType {
+        @JsonProperty(value="file")
+        public String file;
+
+        public FileType(String f) {
+            file = f;
+        }
+    }
+
+    public abstract class Importer {
         public String author;
         public DefTypes.Project project;
+        @JsonProperty(value="endpoint")
+        public Endpoint ep;
         public String endpoint;
+        public Selector select;
         public String selector;
         public String selectorName;
         public String selectorValue;
+        public FileType fileType;
         public String file;
         public Integer timeout;
+
+        public Importer(String ep) {
+            System.out.println("In Importer");
+            this.ep = new Endpoint(ep);
+        }
+
+        public class Selector {
+            @JsonProperty(value="name")
+            public String name;
+            @JsonProperty(value="value")
+            public String value;
+        }
+
+        @JsonProperty(value="selector")
+        public void setSelector(Selector s) {
+            select = s;
+            selectorName = s.name;
+            selectorValue = s.value;
+            selector = String.format("%s='%s'", s.name, s.value);
+        }
+
+        @JsonProperty(value="file")
+        public void setFile(FileType ft) {
+            fileType = ft;
+            file = ft.file;
+        }
     }
 
-    class XUnitImporter extends Importer {
-        Map<String, String> testsuite = new HashMap<>();
+    @JsonRootName(value="xunit")
+    public class XUnitImporter extends Importer {
+        public Map<String, String> testsuite = new HashMap<>();
         // TODO:  Try to find all valid custom fields and make this an enumeration
-        Map<String, String> properties = new HashMap<>();
+        public Map<String, String> properties = new HashMap<>();
+        public TestRun testrun;
         public String testrunId;
         public String testrunTitle;
+        public Custom custom;
+        public CustomProps props;
+
+        public XUnitImporter(String ept) {
+            super(ept);
+            YAMLConfig.this.logger.info("In XUnitImporter constructor");
+        }
+
+        public class TestRun {
+            public String id;
+            public String title;
+        }
+
+        @JsonProperty(value="testrun")
+        public void setTestRun(TestRun tr) {
+            testrun = tr;
+            testrunId = tr.id;
+            testrunTitle = tr.title;
+        }
+
+        public class Custom {
+            @JsonProperty(value="test-suite")
+            public TestSuite testsuite;
+            @JsonProperty(value="properties")
+            public  Properties properties;
+
+            public class TestSuite {
+                List<Map<String, String>> entries;
+                Map<String, String> fields;
+
+                @JsonProperty(value="testsuite")
+                public void setEntries(TestSuite ts) {
+                    YAMLConfig.this.logger.info(ts.toString());
+                }
+            }
+
+            public class Properties {
+
+            }
+        }
+
+        @JsonProperty(value="custom")
+        public void setCustom(Custom c) {
+            custom = c;
+        }
     }
 
-    class TestCaseImporter extends Importer {
+    @JsonRootName(value="testcase")
+    public class TestCaseImporter extends Importer {
+        public Title title;
         public String testrunPrefix;
         public String testrunSuffix;
+
+        public TestCaseImporter(String ep) {
+            super(ep);
+            YAMLConfig.this.logger.info("In TestCaseImporter");
+            this.ep = new Endpoint(ep);
+        }
+
+
+        public class Title {
+            public String prefix;
+            public String suffix;
+        }
+
+        @JsonProperty(value="title")
+        public void setTitle(Title t) {
+            title = t;
+            testrunSuffix = t.suffix;
+            testrunPrefix = t.prefix;
+        }
     }
 
     public void parse(JsonNode root) {
@@ -84,14 +237,20 @@ public class YAMLConfig {
                 case "ossrh":
                     this.serverHandler(key, value);
                     break;
-                case "paths":
-                    this.pathsParser(value);
+                case "testcases":
+                    this.testcasesXMLPath = entry.getValue().textValue();
                     break;
+                case "requirements":
+                    this.requirementsXMLPath = entry.getValue().textValue();
+                    break;
+                case "mapping":
+                    this.mappingPath = entry.getValue().textValue();
                 case "importer":
                     this.importerHandler(value, null);
                     break;
                 default:
                     this.logger.error("Unknown root key: %s", key);
+                    //System.err.println(String.format("Unknown root key: %s", key));
             }
         });
     }
@@ -161,22 +320,20 @@ public class YAMLConfig {
                     this.selectorParser(value, importer);
                     break;
                 case "title":
-                    if (importer instanceof TestCaseImporter)
-                        this.titleParser(value, (TestCaseImporter) importer);
+                    if (importer instanceof TestCaseImporter) this.titleParser(value, (TestCaseImporter) importer);
                     break;
                 case "timeout":
                     importer.timeout = Integer.parseInt(value.toString());
                     break;
                 case "testrun":
-                    if (importer instanceof XUnitImporter)
-                        this.testrunParser(value, (XUnitImporter) importer);
+                    if (importer instanceof XUnitImporter) this.testrunParser(value, (XUnitImporter) importer);
                     break;
                 case "custom":
-                    if (importer instanceof XUnitImporter)
-                        this.customParser(value, (XUnitImporter) importer);
+                    if (importer instanceof XUnitImporter) this.customParser(value, (XUnitImporter) importer);
                     break;
                 default:
                     this.logger.error(String.format("Unknown key for root: %s", key));
+                    //System.err.println(String.format("Unknown key for root: %s", key));
             }
         });
     }
@@ -193,6 +350,7 @@ public class YAMLConfig {
                 val = n.getValue().asText();
             else
                 this.logger.error("Unknown key for selector: %s", n.getKey());
+                //System.err.println(String.format("Unknown key for selector: %s", n.getKey()));
         }
         imp.selectorName = name;
         imp.selectorValue = val;
@@ -211,6 +369,7 @@ public class YAMLConfig {
                     break;
                 default:
                     this.logger.error("Unknown key for testrun: %s", key);
+                    //System.err.println(String.format("Unknown key for testrun: %s", key));
             }
         });
     }
@@ -227,6 +386,7 @@ public class YAMLConfig {
                     break;
                 default:
                     this.logger.error("Unknown key for title: %s", key);
+                    //System.err.println(String.format("Unknown key for title: %s", key));
             }
         });
     }
@@ -248,6 +408,7 @@ public class YAMLConfig {
                             break;
                         default:
                             this.logger.error("Unknown value for xml: %s", entry.getKey());
+                            //System.err.println(String.format("Unknown value for xml: %s", entry.getKey()));
                     }
                 });
             }
@@ -284,39 +445,68 @@ public class YAMLConfig {
                     throw new Error("Must have key of 'test-suites' or 'properties'");
             }
         });
+        ximp.testsuite.entrySet().forEach(es -> {
+            String k = es.getKey();
+            switch(k) {
+                case "dry-run":
+                    ximp.props.dryRun = Boolean.valueOf(es.getValue());
+                    break;
+                case "set-testrun-finished":
+                    ximp.props.setTestRunFinished = Boolean.valueOf(es.getValue());
+                    break;
+                case "include-skipped":
+                    ximp.props.includeSkipped = Boolean.valueOf(es.getValue());
+                    break;
+                default:
+                    this.logger.error("Unknown field for test-suites");
+                    //System.err.println("Unknown field for test-suites");
+            }
+        });
     }
 
-    public static YAMLConfig load(Path configpath) throws IOException {
+    /**
+     * Creates a YAMLConfig object from a given yaml file
+     *
+     * @param configpath path to the yaml file
+     * @return
+     */
+    public static YAMLConfig load(Path configpath) {
         if (configpath == null) {
             String homeDir = System.getProperty("user.home");
-            configpath = FileSystems.getDefault().getPath(homeDir + "/.polarize/yaml-config.yaml");
+            configpath = FileSystems.getDefault().getPath(homeDir + "/.polarize/yaml-config.yml");
         }
         YAMLConfig cfg = new YAMLConfig();
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        JsonNode root = mapper.readTree(configpath.toFile());
-        cfg.parse(root);
+        JsonNode root = null;
+        try {
+            root = mapper.readTree(configpath.toFile());
+            cfg.parse(root);
+        } catch (IOException e) {
+            throw new Error("IOException thrown reading from yaml config file");
+        }
         return cfg;
     }
 
-    public ReporterConfig reportConfig() {
-        ReporterConfig config = new ReporterConfig();
-        Map<String, String> testsuite = this.xunitImporter.testsuite;
-        config.setAuthor(this.author);
-        config.setDryRun(Boolean.valueOf(testsuite.getOrDefault("dry-run", "false")));
-        config.setIncludeSkipped(Boolean.valueOf(testsuite.getOrDefault("include-skipped", "false")));
-        config.setResponseName(this.xunitImporter.selectorValue);
-        config.setSetTestRunFinished(Boolean.valueOf(testsuite.getOrDefault("set-testrun-finished", "true")));
-        config.setTestrunID(this.xunitImporter.testrunId);
-        config.setTestrunTitle(this.xunitImporter.testrunTitle);
-        config.setTestcasesXMLPath(this.testcasesXMLPath);
-        config.setRequirementsXMLPath(this.requirementsXMLPath);
-        config.setResponseTag(this.xunitImporter.selectorName);
-
-        return config;
+    /**
+     *
+     */
+    public void store(File yaml) {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+        try {
+            String text = writer.writeValueAsString(this);
+            //this.logger.info(text);
+            System.out.println(text);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        YAMLConfig cfg = YAMLConfig.load(null);
+        //YAMLConfig cfg = YAMLConfig.load(null);
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        YAMLConfig cfg = mapper.readValue(new File("/home/stoner/.polarize/yaml-config.yml"), YAMLConfig.class);
         System.out.println(cfg.author);
+        cfg.store(null);
     }
 }
