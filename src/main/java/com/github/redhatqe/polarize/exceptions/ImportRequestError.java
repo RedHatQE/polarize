cd73ca598d30294107bb38afd3981c6afd8b4ba2
Sean Toner <stoner@localhost.localdomain>
2016-11-04 01:26:43 -0400

    Added an ImportRequestError type and allowing error message on MappingError

--- src/main/java/com/github/redhatqe/polarize/exceptions/ImportRequestError.java
+++ src/main/java/com/github/redhatqe/polarize/exceptions/ImportRequestError.java
@@ -1,7 +1,12 @@
 package com.github.redhatqe.polarize.exceptions;
 
+import com.github.redhatqe.polarize.importer.ImporterRequest;
+
 /**
  * Created by stoner on 11/2/16.
  */
-public class ImportRequestError {
+public class ImportRequestError extends Error {
+    public ImportRequestError(String err) {
+        super(err);
+    }
 }
--- src/main/java/com/github/redhatqe/polarize/exceptions/MappingError.java
+++ src/main/java/com/github/redhatqe/polarize/exceptions/MappingError.java
@@ -4,4 +4,11 @@ package com.github.redhatqe.polarize.exceptions;
  * Created by stoner on 10/5/16.
  */
 public class MappingError extends Error {
+    public MappingError(String err) {
+        super(err);
+    }
+
+    public MappingError() {
+        super();
+    }
 }
