1b34fc4ea4a868eac4923bbd62c485e1e7b6c09d
Sean Toner <stoner@localhost.localdomain>
2016-11-04 01:25:34 -0400

    Pushed maketimestamp into a Utility class

--- src/main/java/com/github/redhatqe/polarize/Utility.java
+++ src/main/java/com/github/redhatqe/polarize/Utility.java
@@ -1,7 +1,28 @@
 package com.github.redhatqe.polarize;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by stoner on 11/3/16.
  */
 public class Utility {
+    public static String makeTimeStamp() {
+        // Create a timestamped file xml-config-<timestamp>.xml in backup from xml-config.xml
+        LocalDateTime now = LocalDateTime.now();
+        String timestamp = "%s-%s-%d-%d-%d-%d";
+        timestamp = String.format(timestamp, now.getMonth().toString(), now.getDayOfMonth(), now.getYear(),
+                now.getHour(), now.getMinute(), now.getSecond());
+        return timestamp;
+    }
+
+    public static String makeTimeStamp(String base, String end) {
+        // Create a timestamped file xml-config-<timestamp>.xml in backup from xml-config.xml
+        LocalDateTime now = LocalDateTime.now();
+        if (!base.equals(""))
+            base += "-";
+        String timestamp = "%s%s-%s-%d-%d-%d-%d";
+        timestamp = String.format(timestamp, base, now.getMonth().toString(), now.getDayOfMonth(), now.getYear(),
+                now.getHour(), now.getMinute(), now.getSecond());
+        return timestamp.trim() + end;
+    }
 }
