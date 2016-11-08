4144851843fb01d966e37235a4e9c41081aeb7a1
Sean Toner <stoner@localhost.localdomain>
2016-11-08 16:11:44 -0500

    Fixed wrong format symbol

--- src/main/java/com/github/redhatqe/polarize/Utility.java
+++ src/main/java/com/github/redhatqe/polarize/Utility.java
@@ -9,7 +9,7 @@ public class Utility {
     public static String makeTimeStamp() {
         // Create a timestamped file xml-config-<timestamp>.xml in backup from xml-config.xml
         LocalDateTime now = LocalDateTime.now();
-        String timestamp = "%s-%s-%d-%d-%d-%d-d%";
+        String timestamp = "%s-%s-%d-%d-%d-%d-%d";
         timestamp = String.format(timestamp, now.getMonth().toString(), now.getDayOfMonth(), now.getYear(),
                 now.getHour(), now.getMinute(), now.getSecond(), now.getNano());
         return timestamp;
@@ -20,9 +20,13 @@ public class Utility {
         LocalDateTime now = LocalDateTime.now();
         if (!base.equals(""))
             base += "-";
-        String timestamp = "%s%s-%s-%d-%d-%d-%d-d%";
+        String timestamp = "%s%s-%s-%d-%d-%d-%d-%d";
         timestamp = String.format(timestamp, base, now.getMonth().toString(), now.getDayOfMonth(), now.getYear(),
                 now.getHour(), now.getMinute(), now.getSecond(), now.getNano());
         return timestamp.trim() + end;
+    }
+
+    public static String removeLast(String s) {
+        return s.substring(0, s.length() - 1);
     }
 }
