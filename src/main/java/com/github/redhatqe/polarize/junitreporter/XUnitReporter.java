ccea9b8ffa9d59a66423ab08a48aa2f236b6a579
Sean Toner <stoner@localhost.localdomain>
2017-02-01 12:00:00 -0500

    Fixed accounting of pass/fails/skips/errors.  TestNG does not distinguish between fails and errors, but xunit does

--- src/main/java/com/github/redhatqe/polarize/junitreporter/XUnitReporter.java
+++ src/main/java/com/github/redhatqe/polarize/junitreporter/XUnitReporter.java
@@ -15,6 +15,7 @@ import com.github.redhatqe.polarize.importer.xunit.*;
 import com.github.redhatqe.polarize.importer.xunit.Error;
 import com.github.redhatqe.polarize.metadata.Requirement;
 import com.github.redhatqe.polarize.metadata.TestDefinition;
+import com.github.redhatqe.polarize.utils.Tuple;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
@@ -23,7 +24,9 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.*;
+import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
 
 import javax.jms.JMSException;
 
@@ -98,6 +101,30 @@ public class XUnitReporter implements IReporter {
         return props;
     }
 
+    public void setTestSuiteResults(Testsuite ts, FullResult fr, ITestContext ctx) {
+        if (fr.total != fr.fails + fr.errors + fr.skips + fr.passes) {
+            String e = "Total number of tests run != fails + errors + skips + passes\n";
+            String v = "                       %d !=    %d +     %d +    %d +     %d\n";
+            v = String.format(v, fr.total, fr.fails, fr.errors, fr.skips, fr.passes);
+            System.err.println(e + v);
+        }
+
+        // The iterations feature of Polarion means that we don't need to specify how many times a permutation of a
+        // method + args passed/failed/skipped etc.  That's why we don't directly use the fr numbers.
+        int numErrors = fr.errorsByMethod.size();
+        int numFails = ctx.getFailedTests().size();
+        int numSkips = ctx.getSkippedTests().size();
+        int numTotal = ctx.getAllTestMethods().length;
+        if (numErrors > 0)
+            numFails = numFails - numErrors;
+        if (numFails <= 0)
+            numFails = 0;
+        ts.setErrors(Integer.toString(numErrors));
+        ts.setFailures(Integer.toString(numFails));
+        ts.setSkipped(Integer.toString(numSkips));
+        ts.setTests(Integer.toString(numTotal));
+    }
+
 
     /**
      * Generates a modified xunit result that can be used for the XUnit Importer
@@ -123,26 +150,52 @@ public class XUnitReporter implements IReporter {
 
         // Get information for each <testsuite>
         suites.forEach(suite -> {
-            String name = suite.getName();
+            // suite here for the rhsm-qe tests should only be one occurrence
             Map<String, ISuiteResult> results = suite.getResults();
+            Map<String, Tuple<FullResult, List<Testcase>>> full = XUnitReporter.getMethodInfo(suite, this.bad);
             List<Testsuite> collected = results.entrySet().stream()
                     .map(es -> {
+                        // the results that we iterate through is each <test> element from the suite.xml.  From our
+                        // perspective each <testsuite> is effectively the <test>, and in turn we model each <test>
+                        // as a Class in java
                         Testsuite ts = new Testsuite();
+                        List<Testcase> tests = ts.getTestcase();
+
                         String key = es.getKey();
                         ISuiteResult result = es.getValue();
                         ITestContext ctx = result.getTestContext();
+                        XmlTest xt = ctx.getCurrentXmlTest();
+                        List<XmlClass> clses = xt.getClasses();
+                        Set<String> clsSet = clses.stream()
+                                .map(x -> {
+                                    String sptCls = x.getSupportClass().toString();
+                                    sptCls = sptCls.replace("class ", "");
+                                    return sptCls;
+                                })
+                                .collect(Collectors.toSet());
 
                         ts.setName(key);
-                        ts.setFailures(Integer.toString(ctx.getFailedTests().size()));
-                        ts.setSkipped(Integer.toString(ctx.getSkippedTests().size()));
-                        ts.setTests(Integer.toString(ctx.getAllTestMethods().length));
                         Date start = ctx.getStartDate();
                         Date end = ctx.getEndDate();
-                        float duration = (end.getTime() - start.getTime())/ 1000;
-                        ts.setTime(Float.toString(duration));
+                        double duration = (end.getTime() - start.getTime()) / 1000.0;
+                        ts.setTime(Double.toString(duration));
 
-                        List<Testcase> tests = ts.getTestcase();
-                        List<Testcase> after = XUnitReporter.getMethodInfo(suite, tests, this.bad);
+                        // While I suppose it's possible, we should have only one or zero possible results from the map
+                        // so findFirst should return at most 1.
+                        List<Tuple<FullResult, List<Testcase>>> frList = full.entrySet().stream()
+                                .filter(e -> {
+                                    String cls = e.getKey();
+                                    return clsSet.contains(cls);
+                                })
+                                .map(Map.Entry::getValue)
+                                .collect(Collectors.toList());
+                        Optional<Tuple<FullResult, List<Testcase>>> maybeFR = frList.stream().findFirst();
+                        Tuple<FullResult, List<Testcase>> tup = maybeFR.orElse(new Tuple<>());
+                        FullResult fr = tup.first;
+                        List<Testcase> tcs = tup.second;
+                        tests.addAll(tcs);
+
+                        setTestSuiteResults(ts, fr, ctx);
                         return ts;
                     })
                     .collect(Collectors.toList());
@@ -150,6 +203,7 @@ public class XUnitReporter implements IReporter {
         });
 
         // Now that we've gone through the suites, let's marshall this into an XML file for the XUnit Importer
+        getSuiteResults(tsuites);
         File reportPath = new File(outputDirectory + "/testng-polarion.xml");
         JAXBHelper jaxb = new JAXBHelper();
         IJAXBHelper.marshaller(tsuites, reportPath, jaxb.getXSDFromResource(Testsuites.class));
@@ -232,25 +286,33 @@ public class XUnitReporter implements IReporter {
      * @param result
      * @param tc
      */
-    private static void getStatus(ITestResult result, Testcase tc) {
+    private static void getStatus(ITestResult result, Testcase tc, FullResult fr, String qual) {
         Throwable t = result.getThrowable();
         int status = result.getStatus();
+        StringBuilder sb = new StringBuilder();
+        fr.total++;
         switch(status) {
             // Unfortunately, TestNG doesn't distinguish between an assertion failure and an error.  The way to check
             // is if getThrowable() returns non-null
             case ITestResult.FAILURE:
                 if (t != null && !(t instanceof java.lang.AssertionError)) {
+                    fr.errors++;
+                    if (!fr.errorsByMethod.contains(qual))
+                        fr.errorsByMethod.add(qual);
                     Error err = new Error();
-                    String msg = t.getMessage().length() > 128 ? t.getMessage().substring(128) : t.getMessage();
-                    err.setMessage(msg);
-                    StringBuilder sb = new StringBuilder();
-                    Arrays.stream(t.getStackTrace()).forEach(st -> {
-                        sb.append(String.format(st.toString() + "\n"));
-                    });
+                    String maybe = t.getMessage();
+                    if (maybe != null) {
+                        String msg = t.getMessage().length() > 128 ? t.getMessage().substring(128) : t.getMessage();
+                        err.setMessage(msg);
+                    }
+                    else
+                        err.setMessage("java.lang.NullPointerException");
+                    Arrays.stream(t.getStackTrace()).forEach(st -> sb.append(st.toString()).append("\n"));
                     err.setContent(sb.toString());
                     tc.getError().add(err);
                 }
                 else {
+                    fr.fails++;
                     Failure fail = new Failure();
                     if (t != null)
                         fail.setContent(t.getMessage());
@@ -258,9 +320,11 @@ public class XUnitReporter implements IReporter {
                 }
                 break;
             case ITestResult.SKIP:
+                fr.skips++;
                 tc.setSkipped("true");
                 break;
             case ITestResult.SUCCESS:
+                fr.passes++;
                 tc.setStatus("success");
                 break;
             default:
@@ -274,15 +338,54 @@ public class XUnitReporter implements IReporter {
         }
     }
 
+    public static boolean
+    checkMethInMapping(Map<String, IdParams> inner, String qual, String project, File badMethods) {
+        boolean in = true;
+        if (inner == null || !inner.containsKey(project)) {
+            String err = String.format("Project %s does not exist for %s in mapping file\n", project, qual);
+            logger.error(err);
+            System.err.println(err);
+            try {
+                FileWriter badf = new FileWriter(badMethods, true);
+                BufferedWriter bw = new BufferedWriter(badf);
+                bw.write(err);
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            in = false;
+        }
+        return in;
+    }
+
+    public void getSuiteResults(Testsuites suites) {
+        List<Testsuite> sList = suites.getTestsuite();
+        FullResult fr = sList.stream()
+                .reduce(new FullResult(),
+                        (acc, s) -> {
+                            acc.skips += Integer.parseInt(s.getSkipped());
+                            acc.errors += Integer.parseInt(s.getErrors());
+                            acc.fails += Integer.parseInt(s.getFailures());
+                            acc.total += Integer.parseInt(s.getTests());
+                            acc.passes = acc.passes + (acc.total - (acc.skips + acc.errors + acc.fails));
+                            return acc;
+                        },
+                        FullResult::add);
+        //suites.setErrors(Integer.toString(fr.errors));
+        //suites.setTests(Integer.toString(fr.total));
+        //suites.setFailures(Integer.toString(fr.skips));
+    }
+
     /**
      * Gets information from each invoked method in the test suite
      *
      * @param suite suite that was run by TestNG
-     * @param testcases all the Testcase objects
-     * @return modified list of the Testcase objects
+     * @return map of classname to a tuple of the FullResult and TestCase
      */
-    private static List<Testcase> getMethodInfo(ISuite suite, List<Testcase> testcases, File badMethods) {
+    private static Map<String, Tuple<FullResult, List<Testcase>>>
+    getMethodInfo(ISuite suite, File badMethods) {
         List<IInvokedMethod> invoked = suite.getAllInvokedMethods();
+        Map<String, Tuple<FullResult, List<Testcase>>> full = new HashMap<>();
         for(IInvokedMethod meth: invoked) {
             ITestNGMethod fn = meth.getTestMethod();
             if (!fn.isTest()) {
@@ -307,61 +410,71 @@ public class XUnitReporter implements IReporter {
             Map<String, Map<String, IdParams>> mapping = FileHelper.loadMapping(fpath);
             Map<String, IdParams> inner = mapping.get(qual);
 
-            if (inner == null || !inner.containsKey(project)) {
-                String err = String.format("Project %s does not exist for %s in mapping file\n", project, qual);
-                logger.error(err);
-                System.err.println(err);
-                //throw new MappingError(err);
-                try {
-                    FileWriter badf = new FileWriter(badMethods, true);
-                    BufferedWriter bw = new BufferedWriter(badf);
-                    bw.write(err);
-                    bw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (!checkMethInMapping(inner, qual, project, badMethods))
                 continue;
-            }
 
+            FullResult fres;
             Testcase testcase = new Testcase();
-            testcases.add(testcase);
-
-            //XmlTest test = fn.getXmlTest();
+            List<Testcase> tests;
+            if (!full.containsKey(classname)) {
+                fres = new FullResult();
+                tests = new ArrayList<>();
+                tests.add(testcase);
+                full.put(classname, new Tuple<>(fres, tests));
+            }
+            else {
+                Tuple<FullResult, List<Testcase>> tup = full.get(classname);
+                fres = tup.first;
+                tests = tup.second;
+                tests.add(testcase);
+            }
             ITestResult result = meth.getTestResult();
-            Long millis = result.getEndMillis() - result.getStartMillis();
-            testcase.setTime(millis.toString());
+            Double millis = (result.getEndMillis() - result.getStartMillis()) / 1000.0;
 
+            fres.classname = classname;
+            testcase.setTime(millis.toString());
             testcase.setName(methname);
             testcase.setClassname(classname);
+            XUnitReporter.getStatus(result, testcase, fres, qual);
 
-            // Create the <properties> element, and all the child <property> sub-elements
-            com.github.redhatqe.polarize.importer.xunit.Properties props =
-                    new com.github.redhatqe.polarize.importer.xunit.Properties();
-            List<Property> tcProps = props.getProperty();
-            XUnitReporter.getStatus(result, testcase);
-
+            // Create the <properties> element, and all the child <property> sub-elements from the iteration data
             IdParams ip = inner.get(project);
             String id = ip.getId();
             List<String> args = ip.getParameters();
-
             Property polarionID = XUnitReporter.createProperty("polarion-testcase-id", id);
-            tcProps.add(polarionID);
-
-            // Get all the iteration data
-            Object[] params = result.getParameters();
-            if (args.size() != params.length) {
-                XUnitReporter.logger.error("Length of parameters from IResult not the same as from mapping file");
-                throw new MappingError();
-            }
-            for(int x = 0; x < params.length; x++) {
-                Property param = new Property();
-                param.setName("polarion-parameter-" + args.get(x));
-                param.setValue(params[x].toString());
-                tcProps.add(param);
-            }
+            com.github.redhatqe.polarize.importer.xunit.Properties props =
+                    getPropertiesFromMethod(result, args, polarionID);
             testcase.setProperties(props);
         }
-        return testcases;
+        return full;
+    }
+
+    public static com.github.redhatqe.polarize.importer.xunit.Properties
+    getPropertiesFromMethod(ITestResult result, List<String> args, Property polarionID) {
+        com.github.redhatqe.polarize.importer.xunit.Properties props =
+                new com.github.redhatqe.polarize.importer.xunit.Properties();
+        List<Property> tcProps = props.getProperty();
+        tcProps.add(polarionID);
+
+        // Get all the iteration data
+        Object[] params = result.getParameters();
+        if (args.size() != params.length) {
+            XUnitReporter.logger.error("Length of parameters from IResult not the same as from mapping file");
+            throw new MappingError();
+        }
+        for(int x = 0; x < params.length; x++) {
+            Property param = new Property();
+            param.setName("polarion-parameter-" + args.get(x));
+            //param.setName(String.format("polarion-parameter-Arg%d", x));
+            String p;
+            if (params[x] == null)
+                p = "null";
+            else
+                p = params[x].toString();
+            param.setValue(p);
+            tcProps.add(param);
+        }
+        return props;
     }
 
     private static Testsuites getTestSuiteInfo(String responseName) {
