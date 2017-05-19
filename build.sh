b79580ac569cb9f55e8d6c5d955b7a91c8547930
Sean Toner <stoner@localhost.localdomain>
2017-05-19 14:22:55 -0400

    helper script to build polarize

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+gradle clean
+gradle preparePublication 2> /dev/null
+gradle preparePublication
+gradle install
+gradle publishToMavenLocal
+gradle publish
+
+# This is a hack.  Ideally, mavencentral will host the uberjars, but currently gradle publish does not do that
+#scp ./build/libs/polarize-${1}-all.jar ${2}
